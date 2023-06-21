f547184c2f2eca2f886248bc2b3ea1e15908ea90
Stefano Cutri <stefanocutristecco@gmail.com>
2023-06-21 14:55:10 -0300

    SingleMovie Details ready

--- src/components/Movies/MovieDetailsScreen.js
+++ src/components/Movies/MovieDetailsScreen.js
@@ -2,21 +2,32 @@ import { LinearGradient } from "expo-linear-gradient";
 import React, { useEffect } from "react";
 import { View, StyleSheet, TouchableOpacity } from "react-native";
 import { Image, ImageBackground, Text } from "react-native";
-import { ArrowLeftIcon } from "react-native-heroicons/outline";
+import {
+  ArrowLeftIcon,
+  CalendarIcon,
+  ClockIcon,
+  GlobeAltIcon,
+} from "react-native-heroicons/outline";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchOneMovie } from "../../reducers/moviesReducer";
 import StarRating from "react-native-star-rating-widget";
+import ISO6391 from "iso-639-1";
+
+// import "intl/locale-data/jsonp/en";/
 
 const MovieDetailsScreen = ({ route, navigation }) => {
   const dispatch = useDispatch();
   const state = useSelector((state) => state.movies);
 
   const movie = route.params;
+
   const image = {
     uri: `https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`,
   };
+
   useEffect(() => {
     dispatch(fetchOneMovie(movie.id));
+
     navigation.setOptions({
       title: movie.title,
       headerLeft: (props) => (
@@ -31,7 +42,12 @@ const MovieDetailsScreen = ({ route, navigation }) => {
   if (state.singleMovie.genres !== undefined) {
     movieGenres = state.singleMovie.genres.slice(0, 3);
   }
-  console.log(movieGenres);
+  const convertMinsToTime = (mins) => {
+    let hours = Math.floor(mins / 60);
+    let minutes = mins % 60;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    return `${hours}h ${minutes}min`;
+  };
 
   return (
     <View style={styles.container}>
@@ -40,51 +56,128 @@ const MovieDetailsScreen = ({ route, navigation }) => {
           colors={["#00000000", "#000000"]}
           style={{ height: "100%", width: "100%" }}
         ></LinearGradient>
-        <Text
+        {/* Info Container */}
+        <View
           style={{
-            color: "white",
-            fontWeight: "bold",
-            fontSize: 24,
-            marginLeft: 12,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            borderWidth: 0,
+            borderColor: "red",
           }}
         >
-          {movie.title}
-        </Text>
-        <View style={{ flexDirection: "row", marginLeft: 8 }}>
-          {movieGenres.map((genre) => (
-            <View
-              key={genre.name}
+          {/* Left Column */}
+          <View>
+            <Text
               style={{
-                marginLeft: 5,
-                marginTop: 10,
-                backgroundColor: "#fff",
-                borderRadius: 5,
+                color: "white",
+                fontWeight: "bold",
+                fontSize: 20,
+                marginLeft: 12,
+                width: 250,
               }}
             >
-              <Text
-                style={{
-                  color: "#000",
-                  padding: 2,
-                  paddingHorizontal: 5,
-                  fontSize: 13,
+              {movie.title}
+            </Text>
+            <View style={{ flexDirection: "row", marginLeft: 8 }}>
+              {movieGenres.map((genre) => (
+                <View
+                  key={genre.name}
+                  style={{
+                    marginLeft: 5,
+                    marginTop: 10,
+                    backgroundColor: "#fff",
+                    borderRadius: 5,
+                  }}
+                >
+                  <Text
+                    style={{
+                      color: "#000",
+                      padding: 2,
+                      paddingHorizontal: 5,
+                      fontSize: 13,
+                    }}
+                  >
+                    {genre.name}
+                  </Text>
+                </View>
+              ))}
+            </View>
+            <View
+              style={{
+                marginLeft: 8,
+                marginTop: 10,
+                flexDirection: "row",
+                alignItems: "center",
+              }}
+            >
+              <StarRating
+                starSize={22}
+                color="#fff"
+                rating={movie.vote_average / 2}
+                onChange={() => console.log("Rating")}
+                animationConfig={{
+                  scale: 1,
                 }}
-              >
-                {genre.name}
+              />
+              <View style={{ flexDirection: "row" }}>
+                <Text
+                  style={{
+                    fontSize: 15,
+                    ...styles.textWhite,
+                    paddingHorizontal: 5,
+                  }}
+                >
+                  {state.singleMovie.vote_count}
+                </Text>
+                <Text style={{ fontSize: 15, ...styles.textWhite }}>votes</Text>
+              </View>
+            </View>
+          </View>
+          {/* Right Column */}
+          <View style={{ marginRight: 20, marginTop: 6 }}>
+            {/* Release Date */}
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                marginBottom: 5,
+              }}
+            >
+              <CalendarIcon color="#fff" size={14} style={{ marginRight: 3 }} />
+              <Text style={{ ...styles.textWhite, fontSize: 11 }}>
+                {new Date(state.singleMovie.release_date).toLocaleDateString(
+                  "pl",
+                  { day: "numeric", month: "numeric", year: "2-digit" }
+                )}
               </Text>
             </View>
-          ))}
+            {/* Language */}
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                marginBottom: 5,
+              }}
+            >
+              <GlobeAltIcon color="#fff" size={14} style={{ marginRight: 3 }} />
+              <Text style={{ ...styles.textWhite, fontSize: 11 }}>
+                {ISO6391.getName(state.singleMovie.original_language)}
+              </Text>
+            </View>
+            {/* Runtime */}
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <ClockIcon color="#fff" size={14} style={{ marginRight: 3 }} />
+              <Text style={{ ...styles.textWhite, fontSize: 11 }}>
+                {convertMinsToTime(state.singleMovie.runtime)}
+              </Text>
+            </View>
+          </View>
         </View>
-        <View style={{ marginLeft: 8, marginTop: 10 }}>
-          <StarRating
-            starSize={25}
-            color="#fff"
-            rating={movie.vote_average / 2}
-            onChange={() => console.log("Rating")}
-            animationConfig={{
-              scale: 1,
-            }}
-          />
-        </View>
+        <Text
+          style={{ ...styles.textWhite, paddingHorizontal: 14, marginTop: 25 }}
+        >
+          {movie.overview}
+        </Text>
       </ImageBackground>
     </View>
   );
@@ -100,6 +193,9 @@ const styles = StyleSheet.create({
   },
   posterImage: {
     height: 200,
+  },
+  textWhite: {
+    color: "#fff",
   },
 });
 export default MovieDetailsScreen;
